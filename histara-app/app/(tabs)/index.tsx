7d68c638855ec229586f52810f738eabd64a1cbd
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-12 13:29:31 +0700

    feat: add Google Sign In button

--- histara-app/app/(tabs)/index.tsx
+++ histara-app/app/(tabs)/index.tsx
@@ -2,9 +2,11 @@ import { Image, View, Text, TouchableOpacity } from "react-native";
 import { Colors } from "@/constants/Colors";
 import Button from "@/components/Button";
 import CustomText from "@/components/CustomText";
+import { useState } from "react";
 
 export default function HomeScreen() {
-  return (
+  const [touched, setTouched] = useState(false)
+  return ( 
     <View
       style={{
         justifyContent: "flex-start",
@@ -36,9 +38,9 @@ export default function HomeScreen() {
         </Text>
       </View>
 
-      <View style={{ width: "100%", marginTop: 42 }}>
+      <View style={{ width: "100%", marginTop: 42, gap: 16 }}>
         <Button text="Create Account" />
-        <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "center", marginTop: 16 }}>
+        <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "center" }}>
           <CustomText
             weight={400}
             italic={false}
@@ -56,14 +58,63 @@ export default function HomeScreen() {
             </CustomText>
           </TouchableOpacity>
         </View>
-        <View style={{ width: "100%", flexDirection: "row", alignItems: "center", justifyContent: "center", marginTop: 16 }}>
-          <View style={{ flex: 1, width: 20, height: 1, backgroundColor: "#828282 " }} />
-          <View style={{marginHorizontal: 5}}>
-          <CustomText weight={700} style={[{color: "#828282"}]}>OR</CustomText>
+        <View style={{ width: "100%", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+          <View style={{ flex: 1, width: 20, height: 1, backgroundColor: "#828282" }} />
+          <View style={{ marginHorizontal: 5 }}>
+            <CustomText
+              weight={700}
+              style={[{ color: "#828282" }]}
+            >
+              OR
+            </CustomText>
           </View>
-          <View style={{ flex: 1, width: 20, height: 1, backgroundColor: "#828282 " }} />
-        </View> 
-        <Button text="Continue with Google" />
+          <View style={{ flex: 1, width: 20, height: 1, backgroundColor: "#828282" }} />
+        </View>
+        <TouchableOpacity
+          style={[
+            {
+              backgroundColor: "transparent",
+              width: "100%",
+              borderRadius: 8,
+              borderColor: Colors.orange.main,
+              borderWidth: 3,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingVertical: 9,
+              gap: 8
+            },
+            touched && {
+              backgroundColor: "rgba(0,0,0,.2)",
+              opacity: 1,
+              borderColor: Colors.orange.dark
+            }
+          ]}
+          onPress={() => {
+            console.log("Google");
+          }}
+          onPressIn={() => {
+            setTouched(true);
+          }}
+          onPressOut={() => {
+            setTouched(false);
+          }}
+        >
+          <Image
+            source={require("@/assets/images/Google.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <Text
+            style={{
+              color: Colors.blue.dark,
+              fontFamily: "PoppinsBold",
+              fontSize: 16,
+              lineHeight: 1.4 * 16
+            }}
+          >
+            Continue with Google
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
