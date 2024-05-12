4ecfb3a447c6e0fb595481a05bdbd82d97c824b0
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-12 17:11:42 +0700

    feat: create FormTextInput component

---
+++ histara-app/components/FormTextInput.tsx
@@ -0,0 +1,58 @@
+import { Colors } from "@/constants/Colors";
+import { TextInput, View } from "react-native";
+
+interface Props {
+  state: string;
+  setState: (newText: string) => any;
+  placeholder?: string;
+  type?: "text" | "password" | "email";
+}
+
+type autoCapitalizeT = "none" | "sentences" | "words" | "characters" | undefined;
+
+export default function FormTextInput({
+  state,
+  setState = (newText) => {
+    console.log(newText);
+  },
+  placeholder = "Placeholder",
+  type = "text",
+}: Props) {
+  let keyboardType = "default";
+  let autoComplete = "off";
+  let autoCapitalize: autoCapitalizeT = "none";
+
+  if (type === "email") {
+    keyboardType = "email-address";
+    autoComplete = "email";
+  } else if (type === "password") {
+    autoCapitalize = "none"; // passwords should not auto capitalize
+    autoComplete = "password";
+  } else if (type === "text") {
+    autoCapitalize = "sentences"; // capitalize the first letter of each sentence
+  }
+
+  return (
+    <View>
+      <TextInput
+        value={state}
+        onChangeText={(newText) => setState(newText)}
+        placeholder={placeholder}
+        // keyboardType={keyboardType}
+        // autoCapitalize={autoCapitalize}
+        // autoComplete={autoCapitalize}
+        style={{
+          borderColor: Colors.orange.main,
+          borderWidth: 2,
+          borderRadius: 8,
+          fontFamily: "PoppinsBold",
+          paddingHorizontal: 8,
+          paddingTop: 10,
+          paddingBottom: 8,
+          fontSize: 16,
+          lineHeight: 1 * 16
+        }}
+      />
+    </View>
+  );
+}
