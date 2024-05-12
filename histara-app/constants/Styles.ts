48d7be121268544d58c3101d6c3c7188c7569c9a
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-12 15:43:33 +0700

    feat: define often-used styles

---
+++ histara-app/constants/Styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const gs = StyleSheet.create({
+  flexRow : {
+    flexDirection: "row"
+  },
+  flexCol: {
+    flexDirection: "column"
+  },
+  jc : {
+    justifyContent: "center"
+  },
+  ic: {
+    alignItems: "center"
+  }
+})
