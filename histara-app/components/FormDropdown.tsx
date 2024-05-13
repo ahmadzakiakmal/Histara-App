71b477559006575a4d8f8a5a98ca1b0e47a4a140
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-13 13:10:11 +0700

    feat: create FormDropdown component

---
+++ histara-app/components/FormDropdown.tsx
@@ -0,0 +1,76 @@
+import { Image, Text, View } from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
+import CustomText from "./CustomText";
+import { Colors } from "@/constants/Colors";
+import { gs } from "@/constants/Styles";
+import { Dispatch } from "react";
+
+interface Props {
+  state: any;
+  setState: (newValue: any) => void;
+  placeholder: string;
+  options: OptionItem[];
+}
+
+interface OptionItem {
+  title: string;
+  value: any;
+}
+
+export default function FormDropdown({
+  state,
+  setState,
+  placeholder = "Select...",
+  options = [
+    {
+      title: "Test 1",
+      value: 1,
+    },
+    {
+      title: "Test 2",
+      value: 2,
+    },
+  ],
+}: Props) {
+  return (
+    <SelectDropdown
+      data={options}
+      onSelect={(selectedItem, index) => {
+        console.log(selectedItem, index);
+        setState(selectedItem);
+      }}
+      renderButton={(selectedItem, isOpen) => {
+        return (
+          <View
+            style={[
+              gs.flexRow,
+              {
+                borderColor: Colors.orange.main,
+                borderWidth: 2,
+                borderRadius: 8,
+                paddingHorizontal: 8,
+                paddingTop: 10,
+                paddingBottom: 8,
+              },
+            ]}
+          >
+            <CustomText
+              weight={700}
+              style={[{ color: selectedItem ? "#000" : "#DADADA", flex: 1 }]}
+            >
+              {selectedItem?.title ?? placeholder}
+            </CustomText>
+            <Image source={require("@/assets/images/CeretDown.png")} style={{width: 30 }} />
+          </View>
+        );
+      }}
+      renderItem={(item) => {
+        return (
+          <View style={{ paddingVertical: 2, paddingHorizontal: 5 }}>
+            <CustomText weight={500}>{item?.title}</CustomText>
+          </View>
+        );
+      }}
+    />
+  );
+}
--- histara-app/components/auth/SignUp.tsx
+++ histara-app/components/auth/SignUp.tsx
@@ -1,54 +1,64 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import FormTextInput from "../FormTextInput";
 import { Dispatch, useState } from "react";
 import CustomText from "../CustomText";
 import Button from "../Button";
+import FormDropdown from "../FormDropdown";
 
 export default function SignUpTab() {
   const [name, setName]: [string, Dispatch<string>] = useState("");
   const [email, setEmail]: [string, Dispatch<string>] = useState("");
   const [phoneNumber, setPhoneNumber]: [string, Dispatch<string>] = useState("");
-  const [biurthday, setBirthday] = useState();
-  const [gender, setGender] = useState();
+  const [birthday, setBirthday] = useState("");
+  const [gender, setGender] = useState("");
   const [work, setWork] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
-    <View style={{ paddingHorizontal: 24, width: "100%", gap: 10 }}>
+    <View style={{ paddingHorizontal: 24, width: "100%", gap: 10, paddingBottom: 40 }}>
       <FormTextInput
         placeholder="Name"
         state={name}
         setState={(text) => {
-          setName(text)
+          setName(text);
         }}
       />
       <FormTextInput
         placeholder="Email"
         state={email}
         setState={(text) => {
-          setEmail(text)
+          setEmail(text);
         }}
       />
       <FormTextInput
         placeholder="Phone Number"
         state={phoneNumber}
         setState={(text) => {
-          setPhoneNumber(text)
+          setPhoneNumber(text);
         }}
+      />
+      <FormDropdown
+        state={gender}
+        setState={(newValue: any) => setGender(newValue)}
+        options={[
+          { title: "Laki-Laki", value: "Laki-Laki" },
+          { title: "Perempuan", value: "Perempuan" },
+        ]}
+        placeholder="Gender"
       />
       <FormTextInput
         placeholder="Work"
         state={work}
         setState={(text) => {
-          setWork(text)
+          setWork(text);
         }}
       />
       <FormTextInput
         placeholder="Password"
         state={password}
         setState={(text) => {
-          setPassword(text)
+          setPassword(text);
         }}
         type="password"
       />
@@ -56,11 +66,45 @@ export default function SignUpTab() {
         placeholder="Confirm Password"
         state={confirmPassword}
         setState={(text) => {
-          setConfirmPassword(text)
+          setConfirmPassword(text);
         }}
         type="password"
       />
       <Button text="SIGN UP" />
+
+      <View
+        style={{
+          marginTop: 20,
+          borderColor: "#DADADA",
+          borderWidth: 2,
+          width: "100%",
+          borderRadius: 6,
+          paddingVertical: 37,
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+          paddingHorizontal: 9,
+        }}
+      >
+        <Image
+          source={require("@/assets/images/Handshake.png")}
+          style={{ position: "absolute", top: 0, transform: [{ translateY: -37 }] }}
+          width={74}
+          height={74}
+        />
+        <CustomText
+          weight={700}
+          style={[{ color: "#DADADA", fontSize: 18 }]}
+        >
+          We Promise
+        </CustomText>
+        <CustomText
+          weight={400}
+          style={[{ color: "#828282", textAlign: "center", fontSize: 12 }]}
+        >
+          We respect your privacy and will never share your email with 3rd parties
+        </CustomText>
+      </View>
     </View>
   );
 }
