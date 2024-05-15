5bc457988097259ec823dc8c0391e078a9ba7d6b
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-15 19:10:38 +0700

    feat: done Profile Screen (closes #4)

--- histara-app/app/profile.tsx
+++ histara-app/app/profile.tsx
@@ -3,58 +3,111 @@ import Header from "@/components/Header";
 import MiniButton from "@/components/MiniButton";
 import { Colors } from "@/constants/Colors";
 import { gs } from "@/constants/Styles";
-import { Pressable, ScrollView, View } from "react-native";
+import { useState } from "react";
+import { Image, Linking, Pressable, ScrollView, View } from "react-native";
 
 export default function ProfileScreen() {
   return (
     <View style={{ backgroundColor: "#FFF" }}>
       <Header />
-      <ScrollView contentContainerStyle={{ paddingHorizontal: 24, paddingVertical: 35, height: "100%" }}>
-        <View style={[gs.flexRow, gs.ic, { justifyContent: "space-between" }]}>
-          <View>
-            <CustomText
-              weight={700}
-              style={[{ lineHeight: 1 * 20, fontSize: 20 }]}
-            >
-              Nama Lengkap
-            </CustomText>
-            <CustomText
-              weight={500}
-              italic={true}
-              style={[{ fontSize: 15, lineHeigth: 1 * 15, marginTop: -5 }]}
-            >
-              xy points
-            </CustomText>
+      <ScrollView contentContainerStyle={{ paddingTop: 35 }}>
+        <View style={{ paddingHorizontal: 24, paddingBottom: 46 }}>
+          <View style={[gs.flexRow, gs.ic, { justifyContent: "space-between" }]}>
+            <View>
+              <CustomText
+                weight={700}
+                style={[{ lineHeight: 1 * 20, fontSize: 20 }]}
+              >
+                Nama Lengkap
+              </CustomText>
+              <CustomText
+                weight={500}
+                italic={true}
+                style={[{ fontSize: 15, lineHeigth: 1 * 15, marginTop: -5 }]}
+              >
+                xy points
+              </CustomText>
+            </View>
+            <View style={{ backgroundColor: "#DADADA", width: 83, height: 83, borderRadius: 9999 }} />
           </View>
-          <View style={{ backgroundColor: "#DADADA", width: 83, height: 83, borderRadius: 9999 }} />
+
+          <View style={[gs.flexRow, { marginTop: 22.5, gap: 15 }]}>
+            <MiniButton
+              text="Edit Profile"
+              onPress={() => {}}
+              style={[{ flex: 1 }]}
+            />
+            <MiniButton
+              text="Riwayat Tur"
+              onPress={() => {}}
+              style={[{ flex: 1 }]}
+            />
+          </View>
+
+          <View style={{ marginTop: 23, gap: 15 }}>
+            <FormDisplay
+              label="Email"
+              value="poc@histara.com"
+            />
+            <FormDisplay
+              label="Phone Number"
+              value="0812345678910"
+            />
+            <FormDisplay
+              label="Birthday"
+              value="29 Nov 2002"
+            />
+            <FormDisplay
+              label="Gender"
+              value="Laki-laki"
+            />
+            <FormDisplay
+              label="Work"
+              value="Mahasiswa"
+            />
+          </View>
         </View>
 
-        <View style={[gs.flexRow, { marginTop: 22.5, gap: 15 }]}>
-          <MiniButton text="Edit Profile" onPress={() => {}} style={[{flex: 1}]} />
-          <MiniButton text="Riwayat Tur" onPress={() => {}} style={[{flex: 1}]} />
-        </View>
+        <View style={{ paddingHorizontal: 14, paddingTop: 5, backgroundColor: "#F6F6F6" }}>
+          <CustomText
+            weight={700}
+            style={[{ fontSize: 16 }]}
+          >
+            Follow Us
+          </CustomText>
+          <CustomText
+            weight={400}
+            style={[{ fontSize: 16 }]}
+          >
+            Let&apos;s be friends! We love sharing travel inspiration and tips. Follow us to see!
+          </CustomText>
 
-        <View style={{ marginTop: 23, gap: 15 }}>
-          <FormDisplay
-            label="Email"
-            value="poc@histara.com"
-          />
-          <FormDisplay
-            label="Phone Number"
-            value="0812345678910"
-          />
-          <FormDisplay
-            label="Birthday"
-            value="29 Nov 2002"
-          />
-          <FormDisplay
-            label="Gender"
-            value="Laki-laki"
-          />
-          <FormDisplay
-            label="Work"
-            value="Mahasiswa"
-          />
+          <View style={{ paddingBottom: 140, gap: 12 }}>
+            <View style={[gs.flexRow, { gap: 17 }]}>
+              <SocialMediaItem
+                text="Instagram"
+                image={require("@/assets/images/Instagram.png")}
+                link="https://instagram.com"
+              />
+              <SocialMediaItem
+                text="TikTok"
+                image={require("@/assets/images/Tiktok.png")}
+                link="https://tiktok.com"
+              />
+            </View>
+            <View style={[gs.flexRow, { gap: 17 }]}>
+              <SocialMediaItem
+                text="YouTube"
+                image={require("@/assets/images/Youtube.png")}
+                link="https://youtube.com"
+              />
+              <SocialMediaItem
+                text="Facebook"
+                image={require("@/assets/images/Facebook.png")}
+                link="https://facebook.com"
+              />
+            </View>
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -81,5 +134,50 @@ function FormDisplay({ label, value }: FormDisplayProps) {
         {value}
       </CustomText>
     </View>
+  );
+}
+
+interface SocialMediaItemProps {
+  text: string;
+  link: string;
+  image: any;
+}
+function SocialMediaItem({ text, link, image }: SocialMediaItemProps) {
+  const [touched, setTouched] = useState(false)
+  return (
+    <Pressable
+      style={[
+        gs.flexRow,
+        gs.ic,
+        {
+          backgroundColor: touched ? "#E6E6E6" : "#FFF",
+          borderRadius: 6,
+          paddingHorizontal: 19,
+          paddingVertical: 9,
+          gap: 13,
+          flex: 1,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 5,
+          },
+          shadowOpacity: 0.9,
+          shadowRadius: 5,
+          elevation: 5
+        },
+      ]}
+      onPress={() => {
+        Linking.openURL(link).catch((err) => console.log(err));
+      }}
+      onPressIn={() => {
+        setTouched(true)
+      }}
+      onPressOut={() => {
+        setTouched(false)
+      }}
+    >
+      {image && <Image source={image} />}
+      <CustomText weight={400}>{text}</CustomText>
+    </Pressable>
   );
 }
