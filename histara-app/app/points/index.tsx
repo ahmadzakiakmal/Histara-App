721b54b8cf74bd5072553a1a3e4fb8ffc4979e38
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-18 08:54:12 +0700

    feat: partially done Points Screen (#7)

--- histara-app/app/points/index.tsx
+++ histara-app/app/points/index.tsx
@@ -1,5 +1,40 @@
+import CircularProgress from "@/components/CircularProgress";
+import Header from "@/components/Header";
+import { Colors } from "@/constants/Colors";
+import { gs } from "@/constants/Styles";
+import { Image, ScrollView, View } from "react-native";
+import CustomText from "@/components/CustomText";
+
 export default function Points() {
-  return(
-    <></>
-  )
+  return (
+    <>
+      <Header />
+      <ScrollView>
+        <View
+          style={[
+            gs.jc,
+            gs.ic,
+            { backgroundColor: "#4A5379", position: "relative", paddingVertical: 14, overflow: "hidden" },
+          ]}
+        >
+          <Image
+            source={require("@/assets/images/DiamondBlur.png")}
+            style={{ position: "absolute" }}
+          />
+          <CircularProgress
+            color={Colors.orange.main}
+            size={200}
+            strokeWidth={15}
+            percentage={60}
+            duration={200}
+          />
+        </View>
+
+        <View style={{ paddingHorizontal: 18, marginTop: 15 }}>
+          <CustomText weight={700}>Redeem your points</CustomText>
+          <CustomText weight={400}>Your reward after joining the historical tour</CustomText>
+        </View>
+      </ScrollView>
+    </>
+  );
 }
---
+++ histara-app/components/CircularProgress.tsx
@@ -0,0 +1,86 @@
+import React, { useRef, useEffect } from "react";
+import { View, StyleSheet, Animated, Text } from "react-native";
+import Svg, { G, Circle } from "react-native-svg";
+import CustomText from "./CustomText";
+import { gs } from "@/constants/Styles";
+
+type CircularProgressProps = {
+  size: number;
+  strokeWidth: number;
+  percentage: number;
+  duration: number;
+  color: string;
+};
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+
+const CircularProgress: React.FC<CircularProgressProps> = ({ size, strokeWidth, percentage, duration, color }) => {
+  const radius = (size - strokeWidth) / 2;
+  const circumference = radius * 2 * Math.PI;
+  const halfCircle = radius + strokeWidth / 2;
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: percentage,
+      duration: duration,
+      useNativeDriver: false, // useNativeDriver: false for non-UI components
+    }).start();
+  }, [percentage, duration]);
+
+  const strokeDashoffset = animatedValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: [circumference, 0],
+  });
+
+  return (
+    <View style={[gs.ic, gs.jc, { width: size + 20, height: size + 20, backgroundColor: "#FFF", borderRadius: 9999 }]}>
+      <Svg
+        width={size}
+        height={size}
+        viewBox={`0 0 ${size} ${size}`}
+      >
+        <G
+          rotation="-90"
+          origin={`${halfCircle}, ${halfCircle}`}
+        >
+          <Circle
+            cx="50%"
+            cy="50%"
+            r={radius}
+            stroke={color}
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            strokeOpacity={0.2}
+          />
+          <AnimatedCircle
+            cx="50%"
+            cy="50%"
+            r={radius}
+            stroke={color}
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            strokeDasharray={circumference}
+            strokeDashoffset={strokeDashoffset}
+            strokeLinecap="round"
+          />
+        </G>
+      </Svg>
+      <View style={[StyleSheet.absoluteFillObject, gs.ic, gs.jc]}>
+        <CustomText
+          weight={700}
+          style={[{ fontSize: 43, color, lineHeight: 1.1 * 43 }]}
+        >{`${percentage}`}</CustomText>
+        <CustomText
+          weight={400}
+          style={[{ fontSize: 28, color: "rgba(233,148,92,.53)", lineHeight: 1.1 * 28 }]}
+        >
+          points
+        </CustomText>
+      </View>
+    </View>
+  );
+};
+
+export default CircularProgress;
