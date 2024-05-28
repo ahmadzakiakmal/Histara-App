4de87a5c81fe0fdd1a00a676e02d2dc848eba779
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-28 11:06:59 +0700

    fix: add paddingBottom
    to prevent Navbar from hiding it

--- histara-app/app/home/menu-paket/[city].tsx
+++ histara-app/app/home/menu-paket/[city].tsx
@@ -11,7 +11,7 @@ import { panHandlerName } from "react-native-gesture-handler/lib/typescript/hand
 export default function MenuPaketScreen() {
   const { city } = useLocalSearchParams();
   return (
-    <View style={{backgroundColor: "#FFF"}}>
+    <View style={{ backgroundColor: "#FFF" }}>
       <Header />
       <ScrollView>
         <View style={{ height: 200, backgroundColor: Colors.orange.main }}></View>
@@ -22,24 +22,51 @@ export default function MenuPaketScreen() {
         >
           Let&apos;s Explore: {city}
         </CustomText>
-        <Paket title="Paket Keraton Apa Gitu Ngabs" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!" />
-        <Paket title="Paket Keraton" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!" />
-        <Paket title="Paket Keraton" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!" />
+        <View style={{ paddingBottom: 120 }}>
+          <Paket
+            title="Paket Keraton Apa Gitu Ngabs"
+            desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!"
+          />
+          <Paket
+            title="Paket Keraton"
+            desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!"
+          />
+          <Paket
+            title="Paket Keraton"
+            desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam cum assumenda esse vel delectus quos aut odit cupiditate? Sapiente!"
+          />
+        </View>
       </ScrollView>
     </View>
   );
 }
 
 function Paket({ title, desc }: { title: string; desc: string }) {
-  return(
-    <View style={{padding: 18}}>
-      <View style={{backgroundColor: Colors.orange.main, height: 143, borderRadius: 20}} />
-      <View style={[gs.flexRow, {paddingLeft: 18, marginTop: 8, justifyContent: "space-between", alignItems: "flex-end"}]}>
-        <CustomText weight={700} style={[{lineHeight: 1.5 * 18, fontSize: 18, maxWidth: (Utilities.getScreenWidth() - 36)/2}]}>{title ?? "Nama Paket Tur"}</CustomText>
-        <Button style={[{width: "auto", paddingHorizontal: 16, flexShrink: 0}]} textStyle={[{fontFamily: "PoppinsRegular"}]} text="See detail" />
+  return (
+    <View style={{ padding: 18 }}>
+      <View style={{ backgroundColor: Colors.orange.main, height: 143, borderRadius: 20 }} />
+      <View
+        style={[gs.flexRow, { paddingLeft: 18, marginTop: 8, justifyContent: "space-between", alignItems: "flex-end" }]}
+      >
+        <CustomText
+          weight={700}
+          style={[{ lineHeight: 1.5 * 18, fontSize: 18, maxWidth: (Utilities.getScreenWidth() - 36) / 2 }]}
+        >
+          {title ?? "Nama Paket Tur"}
+        </CustomText>
+        <Button
+          style={[{ width: "auto", paddingHorizontal: 16, flexShrink: 0 }]}
+          textStyle={[{ fontFamily: "PoppinsRegular" }]}
+          text="See detail"
+        />
       </View>
-      <CustomText weight={400} style={[{paddingLeft: 18, marginTop: 8}]}>{desc ?? "Desc"}</CustomText>
-      <View style={{height: 1, backgroundColor: "#000", marginTop: 8}} />
+      <CustomText
+        weight={400}
+        style={[{ paddingLeft: 18, marginTop: 8 }]}
+      >
+        {desc ?? "Desc"}
+      </CustomText>
+      <View style={{ height: 1, backgroundColor: "#000", marginTop: 8 }} />
     </View>
-  )
+  );
 }
