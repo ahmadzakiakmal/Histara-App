2c9f10e8677a90cdfb225116b9cb92cf8b9124d9
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-01 20:32:32 +0700

    fix: temporary eslint errors

---
+++ histara-app/app/home/tour/index.tsx
@@ -0,0 +1,36 @@
+import Button from "@/components/Button";
+import CustomText from "@/components/CustomText";
+import { Colors } from "@/constants/Colors";
+import { gs } from "@/constants/Styles";
+import { View } from "react-native";
+import WebView from "react-native-webview";
+
+export default function Tour() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={[gs.flexRow, gs.ic, { backgroundColor: Colors.blue.dark, paddingTop: 50, paddingBottom: 12, paddingHorizontal: 18, gap: 18 }]}>
+        <View style={{ backgroundColor: "#DEDEDE", width: 57, height: 57, borderRadius: 999 }} />
+        <CustomText
+          weight={700}
+          style={[{ color: "#FFF", fontSize: 20 }]}
+        >
+          Nama Paket Tur
+        </CustomText>
+      </View>
+      <WebView
+        containerStyle={{ width: "auto" }}
+        source={{
+          uri: "http://192.168.137.1:5500",
+        }}
+      />
+      <View style={{ backgroundColor: Colors.blue.dark, paddingTop: 50 }}>
+        <CustomText
+          weight={700}
+          style={[{ color: "#FFF" }]}
+        >
+          Nama Paket Tur
+        </CustomText>
+      </View>
+    </View>
+  );
+}
