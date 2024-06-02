c878148c4aa1790588479fb995b9cf222e64f62d
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-02 07:25:23 +0700

    feat: add control buttons and slider on Tour Screen (#18)

--- histara-app/app/home/tour/index.tsx
+++ histara-app/app/home/tour/index.tsx
@@ -2,13 +2,20 @@ import Button from "@/components/Button";
 import CustomText from "@/components/CustomText";
 import { Colors } from "@/constants/Colors";
 import { gs } from "@/constants/Styles";
-import { View } from "react-native";
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import WebView from "react-native-webview";
+import SlidingUpPanel from "rn-sliding-up-panel";
 
 export default function Tour() {
   return (
     <View style={{ flex: 1 }}>
-      <View style={[gs.flexRow, gs.ic, { backgroundColor: Colors.blue.dark, paddingTop: 50, paddingBottom: 12, paddingHorizontal: 18, gap: 18 }]}>
+      <View
+        style={[
+          gs.flexRow,
+          gs.ic,
+          { backgroundColor: Colors.blue.dark, paddingTop: 50, paddingBottom: 12, paddingHorizontal: 18, gap: 18 },
+        ]}
+      >
         <View style={{ backgroundColor: "#DEDEDE", width: 57, height: 57, borderRadius: 999 }} />
         <CustomText
           weight={700}
@@ -23,14 +30,51 @@ export default function Tour() {
           uri: "https://histara-map-git-main-ahmadzaki2975s-projects.vercel.app",
         }}
       />
-      <View style={{ backgroundColor: Colors.blue.dark, paddingTop: 50 }}>
-        {/* <CustomText
-          weight={700}
-          style={[{ color: "#FFF" }]}
+      <View style={{ backgroundColor: Colors.blue.dark, paddingBottom: 10 }}>
+        <Pressable
+          /* @ts-ignore */
+          onPress={() => this._panel.show(360)}
+          style={{ paddingVertical: 5, backgroundColor: Colors.blue.dark, paddingHorizontal: 100 }}
         >
-          
-        </CustomText> */}
+          <View style={{ backgroundColor: "#FFF", height: 5, borderRadius: 999 }} />
+        </Pressable>
+        <View style={[gs.flexRow, gs.jc, {paddingHorizontal: 12, gap: 50}]}>
+          <TouchableOpacity>
+            <Image source={require("@/assets/images/Rewind.png")} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require("@/assets/images/Pause.png")} />
+          </TouchableOpacity> 
+          <TouchableOpacity>
+            <Image source={require("@/assets/images/AntiRewind.png")} />
+          </TouchableOpacity>
+        </View>
       </View>
+      {/* @ts-ignore */}
+      <SlidingUpPanel ref={(c) => (this._panel = c)}>
+        <View
+          style={{
+            backgroundColor: "white",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            paddingVertical: 50,
+            paddingHorizontal: 18,
+            flex: 1,
+          }}
+        >
+          <Text>Here is the content inside panel</Text>
+          <Button
+            text="Hide"
+            onPress={() =>
+              /* @ts-ignore */
+              this._panel.hide()
+            }
+          />
+        </View>
+      </SlidingUpPanel>
+      {/* <View style={{ backgroundColor: Colors.blue.dark, paddingTop: 50 }}>
+        
+      </View> */}
     </View>
   );
 }
