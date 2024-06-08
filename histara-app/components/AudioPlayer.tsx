f90a01e45e5cdd0e462b6c9d0cfc8271c99d7152
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-09 06:57:43 +0700

    feat: add Play button

---
+++ histara-app/components/AudioPlayer.tsx
@@ -0,0 +1,34 @@
+import { gs } from "@/constants/Styles";
+import React, { useEffect, useState } from "react";
+import { View, TouchableOpacity, Image } from "react-native";
+
+export default function AudioPlayer()  {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function handlePlayPause() {
+    setIsPlaying(!isPlaying)
+  }
+
+  return (
+    <View style={[gs.flexRow, gs.jc, { paddingHorizontal: 12, gap: 50, paddingVertical: 12 }]}>
+      <TouchableOpacity
+      // onPress={
+      //   handleRewind
+      // }
+      >
+        <Image source={require("@/assets/images/Rewind.png")} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => handlePlayPause()}
+      >
+        <Image  source={isPlaying ? require("@/assets/images/Pause.png") : require("@/assets/images/Play.png")} />
+      </TouchableOpacity>
+      <TouchableOpacity
+      // onPress={handleForward}
+      >
+        <Image source={require("@/assets/images/AntiRewind.png")} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
